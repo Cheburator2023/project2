b75bab1d1d13e50b7977893b252e2f649e64ee38
synikolaev <synikolaev@inno.tech>
2025-06-11 22:38:49 +0300

    refactor: inter font added

--- apps/react-client/webpack.prod.js
+++ apps/react-client/webpack.prod.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 const { DefinePlugin } = webpack;
 const common = require("./webpack.common.js");
 const APP_NAME = process.env.APP_NAME || "smartAnketa";
+console.log("🐸 Pepe said >> APP_NAME:", APP_NAME);
 
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
 const git_revision = require("child_process")
