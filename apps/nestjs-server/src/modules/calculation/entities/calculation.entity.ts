3d45ec91cbdd1dbd131ea025d8c0940c7485caff
Victor Gabbasov <borisborisov03102001@gmail.com>
2025-07-08 19:28:47 +0300

    feat: добавил новые поля, и получение автора из Кейклоак

--- apps/nestjs-server/src/modules/calculation/entities/calculation.entity.ts
+++ apps/nestjs-server/src/modules/calculation/entities/calculation.entity.ts
@@ -1,24 +1,42 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { CalculationQuestionnaireDataDto } from "../dto/response/calculation-response.dto";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {CalculationQuestionnaireDataDto} from "../dto/response/calculation-response.dto";
 
 @Entity()
 export class Calculation {
-	@PrimaryGeneratedColumn("uuid")
-	id: string;
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
 
-	@Column({ type: "varchar", length: 255, nullable: false })
-	name: string;
+    @Column({type: "varchar", length: 255, nullable: false})
+    name: string;
 
-	@Column({ type: "jsonb", nullable: false })
-	questionnaireData: CalculationQuestionnaireDataDto;
+    @Column({type: "varchar", length: 255, nullable: true})
+    rfd: string;
 
-	@Column({ type: "float", nullable: false })
-	finalCoefficient: number;
+    @Column({type: "varchar", length: 255, nullable: true})
+    streamExecutor: string;
 
-	@Column({
-		type: "timestamp",
-		default: () => "CURRENT_TIMESTAMP",
-		nullable: false,
-	})
-	createdAt: Date;
+    @Column({type: "jsonb", nullable: true})
+    department: string[];
+
+    @Column({type: "varchar", length: 255, nullable: true})
+    customerName: string;
+
+    @Column({type: "varchar", length: 255, nullable: true})
+    comment: string;
+
+    @Column({type: "jsonb", nullable: false})
+    questionnaireData: CalculationQuestionnaireDataDto;
+
+    @Column({type: "float", nullable: false})
+    finalCoefficient: number;
+
+    @Column({
+        type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP",
+        nullable: false,
+    })
+    createdAt: Date;
+
+    @Column({type: "varchar", length: 255, nullable: true})
+    author: string;
 }
