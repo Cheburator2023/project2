33834df4738b0852b5950738555fd5bbd61abca6
synikolaev <synikolaev@inno.tech>
2025-08-05 16:49:35 +0300

    fix: commitlint config

--- .commitlint.config.cjs
+++ .commitlint.config.cjs
@@ -1,6 +1,13 @@
+const pullRequestRegexp = /^Merge pull request #\d+:.*/s;
+
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  	rules: {
+	extends: ["@commitlint/config-conventional"],
+	ignores: [
+		(commit) => pullRequestRegexp.test(commit),
+		(commit) => commit.startsWith("Merge: fix conflict"),
+	],
+	rules: {
 		"body-max-line-length": [1, "always", 1000],
+		"header-max-length": [2, "always", 1000],
 	},
-}; 
+};
--- .commitlint.config.js
+++ .commitlint.config.js
@@ -2,8 +2,12 @@ const pullRequestRegexp = /^Merge pull request #\d+:.*/s;
 
 module.exports = {
 	extends: ["@commitlint/config-conventional"],
-	ignores: [(commit) => pullRequestRegexp.test(commit)],
+	ignores: [
+		(commit) => pullRequestRegexp.test(commit),
+		(commit) => commit.startsWith("Merge: fix conflict"),
+	],
 	rules: {
 		"body-max-line-length": [1, "always", 1000],
+		"header-max-length": [2, "always", 1000],
 	},
 };
