1c39b23cf60a80d1810d15b7a89a938885c16fd2
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-08-26 11:12:10 +0300

    fix: удалил для многих ролей фильтрацию по стримам

--- apps/nestjs-server/src/shared/constants/roles.constant.ts
+++ apps/nestjs-server/src/shared/constants/roles.constant.ts
@@ -1,10 +1,6 @@
 export const STREAM_FILTERED_ROLES = [
 	"ds",
 	"ds_lead",
-	"de",
-	"de_lead",
-	"modelops",
-	"modelops_lead",
 ] as const;
 
 export type StreamFilteredRole = (typeof STREAM_FILTERED_ROLES)[number];
